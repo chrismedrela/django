d137ea16bacc0453687491e696d6557aedc29dd9
Christopher Medrela <chris.medrela@gmail.com>
2012-12-31 19:32:05 +0100

    rewritten test_templates in tests.py

--- django/template/base.py
+++ django/template/base.py
@@ -5,6 +5,7 @@ from functools import partial
 from inspect import getargspec
 
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.template.context import (Context, RequestContext,
     ContextPopException)
 from django.utils.importlib import import_module
