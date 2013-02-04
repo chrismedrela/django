5ea44c02974c9f362fb515bf8471479a19bed008
Christopher Medrela <chris.medrela@gmail.com>
2013-02-04 11:16:29 +0100

    rewritten custom.CustomTagTests.test_assignment_tags

--- tests/regressiontests/templates/custom.py
+++ tests/regressiontests/templates/custom.py
@@ -319,92 +319,140 @@ class CustomTagTests(TestCase):
         self.assert_render(template_source, 'True\n', context)
 
     def test_assignment_tags(self):
-        c = template.Context({'value': 42})
+        tests = [
+            ('{% load custom %}{% assignment_no_params as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_no_params - '
+             'Expected result'),
 
-        t = template.Template('{% load custom %}{% assignment_no_params as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_no_params - Expected result')
+            ('{% load custom %}{% assignment_one_param 37 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_one_param - '
+             'Expected result: 37'),
 
-        t = template.Template('{% load custom %}{% assignment_one_param 37 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_one_param - Expected result: 37')
+            ('{% load custom %}{% assignment_explicit_no_context 37 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_explicit_no_context - '
+             'Expected result: 37'),
 
-        t = template.Template('{% load custom %}{% assignment_explicit_no_context 37 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_explicit_no_context - Expected result: 37')
+            ('{% load custom %}{% assignment_no_params_with_context as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_no_params_with_context - '
+             'Expected result (context value: 42)'),
 
-        t = template.Template('{% load custom %}{% assignment_no_params_with_context as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_no_params_with_context - Expected result (context value: 42)')
+            ('{% load custom %}{% assignment_params_and_context 37 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_params_and_context - '
+             'Expected result (context value: 42): 37'),
 
-        t = template.Template('{% load custom %}{% assignment_params_and_context 37 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_params_and_context - Expected result (context value: 42): 37')
+            ('{% load custom %}{% assignment_two_params 37 42 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_two_params - '
+             'Expected result: 37, 42'),
 
-        t = template.Template('{% load custom %}{% assignment_two_params 37 42 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_two_params - Expected result: 37, 42')
+            ('{% load custom %}{% assignment_one_default 37 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_one_default - '
+             'Expected result: 37, hi'),
 
-        t = template.Template('{% load custom %}{% assignment_one_default 37 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_one_default - Expected result: 37, hi')
+            ('{% load custom %}{% assignment_one_default 37 two="hello" as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_one_default - '
+             'Expected result: 37, hello'),
 
-        t = template.Template('{% load custom %}{% assignment_one_default 37 two="hello" as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_one_default - Expected result: 37, hello')
+            ('{% load custom %}{% assignment_one_default one=99 two="hello" as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_one_default - '
+             'Expected result: 99, hello'),
 
-        t = template.Template('{% load custom %}{% assignment_one_default one=99 two="hello" as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_one_default - Expected result: 99, hello')
+            ('{% load custom %}{% assignment_one_default 37 42 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_one_default - '
+             'Expected result: 37, 42'),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_one_default' received unexpected keyword argument 'three'",
-            template.Template, '{% load custom %}{% assignment_one_default 99 two="hello" three="foo" as var %}')
+            ('{% load custom %}{% assignment_unlimited_args 37 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_unlimited_args - '
+             'Expected result: 37, hi'),
 
-        t = template.Template('{% load custom %}{% assignment_one_default 37 42 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_one_default - Expected result: 37, 42')
+            ('{% load custom %}{% assignment_unlimited_args 37 42 56 89 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_unlimited_args - '
+             'Expected result: 37, 42, 56, 89'),
 
-        t = template.Template('{% load custom %}{% assignment_unlimited_args 37 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_unlimited_args - Expected result: 37, hi')
+            ('{% load custom %}{% assignment_only_unlimited_args as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_only_unlimited_args - '
+             'Expected result: '),
 
-        t = template.Template('{% load custom %}{% assignment_unlimited_args 37 42 56 89 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_unlimited_args - Expected result: 37, 42, 56, 89')
+            ('{% load custom %}{% assignment_only_unlimited_args 37 42 56 89 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_only_unlimited_args - '
+             'Expected result: 37, 42, 56, 89'),
 
-        t = template.Template('{% load custom %}{% assignment_only_unlimited_args as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_only_unlimited_args - Expected result: ')
+            ('{% load custom %}{% assignment_unlimited_args_kwargs '
+             '37 40|add:2 56 eggs="scrambled" four=1|add:3 as var %}'
+             'The result is: {{ var }}',
+             'The result is: assignment_unlimited_args_kwargs - '
+             'Expected result: 37, 42, 56 / eggs=scrambled, four=4'),
+        ]
 
-        t = template.Template('{% load custom %}{% assignment_only_unlimited_args 37 42 56 89 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_only_unlimited_args - Expected result: 37, 42, 56, 89')
+        for template_content, expected_output in tests:
+            self.assert_render(template_content, expected_output)
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_one_param' tag takes at least 2 arguments and the second last argument must be 'as'",
-            template.Template, '{% load custom %}{% assignment_one_param 37 %}The result is: {{ var }}')
+        tests = [
+            ('{% load custom %}{% assignment_one_default '
+             '99 two="hello" three="foo" as var %}',
+             "'assignment_one_default' received unexpected keyword argument 'three'"),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_one_param' tag takes at least 2 arguments and the second last argument must be 'as'",
-            template.Template, '{% load custom %}{% assignment_one_param 37 as %}The result is: {{ var }}')
+            ('{% load custom %}{% assignment_one_param 37 %}'
+             'The result is: {{ var }}',
+             "'assignment_one_param' tag takes at least 2 arguments "
+             "and the second last argument must be 'as'"),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_one_param' tag takes at least 2 arguments and the second last argument must be 'as'",
-            template.Template, '{% load custom %}{% assignment_one_param 37 ass var %}The result is: {{ var }}')
+            ('{% load custom %}{% assignment_one_param 37 as %}'
+             'The result is: {{ var }}',
+             "'assignment_one_param' tag takes at least 2 arguments "
+             "and the second last argument must be 'as'"),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_two_params' received too many positional arguments",
-            template.Template, '{% load custom %}{% assignment_two_params 37 42 56 as var %}The result is: {{ var }}')
+            ('{% load custom %}{% assignment_one_param 37 ass var %}'
+             'The result is: {{ var }}',
+             "'assignment_one_param' tag takes at least 2 arguments "
+             "and the second last argument must be 'as'"),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_one_default' received too many positional arguments",
-            template.Template, '{% load custom %}{% assignment_one_default 37 42 56 as var %}The result is: {{ var }}')
+            ('{% load custom %}{% assignment_two_params 37 42 56 as var %}'
+             'The result is: {{ var }}',
+             "'assignment_two_params' received too many positional arguments"),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_one_default' did not receive value\(s\) for the argument\(s\): 'one'",
-            template.Template, '{% load custom %}{% assignment_one_default as var %}The result is: {{ var }}')
+            ('{% load custom %}{% assignment_one_default 37 42 56 as var %}'
+             'The result is: {{ var }}',
+             "'assignment_one_default' received too many positional arguments"),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_unlimited_args' did not receive value\(s\) for the argument\(s\): 'one'",
-            template.Template, '{% load custom %}{% assignment_unlimited_args as var %}The result is: {{ var }}')
+            ('{% load custom %}{% assignment_one_default as var %}'
+             'The result is: {{ var }}',
+             "'assignment_one_default' did not receive value\(s\) "
+             "for the argument\(s\): 'one'"),
 
-        t = template._Template(engine, '{% load custom %}{% assignment_unlimited_args_kwargs 37 40|add:2 56 eggs="scrambled" four=1|add:3 as var %}The result is: {{ var }}')
-        self.assertEqual(t.render(c), 'The result is: assignment_unlimited_args_kwargs - Expected result: 37, 42, 56 / eggs=scrambled, four=4')
+            ('{% load custom %}{% assignment_unlimited_args as var %}'
+             'The result is: {{ var }}',
+             "'assignment_unlimited_args' did not receive value\(s\) "
+             "for the argument\(s\): 'one'"),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_unlimited_args_kwargs' received some positional argument\(s\) after some keyword argument\(s\)",
-            template.Template, '{% load custom %}{% assignment_unlimited_args_kwargs 37 40|add:2 eggs="scrambled" 56 four=1|add:3 as var %}The result is: {{ var }}')
+            ('{% load custom %}{% assignment_unlimited_args_kwargs '
+             '37 40|add:2 eggs="scrambled" 56 four=1|add:3 as var %}'
+             'The result is: {{ var }}',
+             "'assignment_unlimited_args_kwargs' received "
+             "some positional argument\(s\) after some keyword argument\(s\)"),
 
-        six.assertRaisesRegex(self, template.TemplateSyntaxError,
-            "'assignment_unlimited_args_kwargs' received multiple values for keyword argument 'eggs'",
-            template.Template, '{% load custom %}{% assignment_unlimited_args_kwargs 37 eggs="scrambled" eggs="scrambled" as var %}The result is: {{ var }}')
+            ('{% load custom %}{% assignment_unlimited_args_kwargs '
+             '37 eggs="scrambled" eggs="scrambled" as var %}'
+             'The result is: {{ var }}',
+             "'assignment_unlimited_args_kwargs' received multiple values "
+             "for keyword argument 'eggs'"),
+        ]
+
+        for template_content, error_message_regex in tests:
+            self.assert_compilation_failes(template_content, error_message_regex)
 
     def test_assignment_tag_registration(self):
         # Test that the decorators preserve the decorated function's docstring, name and attributes.
