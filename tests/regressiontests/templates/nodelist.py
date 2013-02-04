61c308abfb64968a7c3cd9b812b55640f8f310e4
Christopher Medrela <chris.medrela@gmail.com>
2013-02-04 17:03:24 +0100

    rewritten nodelist.ErrorIndexTest so it doesn't rely on default_engine

--- tests/regressiontests/templates/nodelist.py
+++ tests/regressiontests/templates/nodelist.py
@@ -28,25 +28,50 @@ class NodelistTest(TestCase):
 
 
 class ErrorIndexTest(TestCase):
-    """
-    Checks whether index of error is calculated correctly in
-    template debugger in for loops. Refs ticket #5831
-    """
+    """ Checks whether index of error is calculated correctly in template
+    debugger in for loops. Refs ticket #5831."""
+
     @override_settings(DEBUG=True, TEMPLATE_DEBUG = True)
     def test_correct_exception_index(self):
         tests = [
-            ('{% load bad_tag %}{% for i in range %}{% badsimpletag %}{% endfor %}', (38, 56)),
-            ('{% load bad_tag %}{% for i in range %}{% for j in range %}{% badsimpletag %}{% endfor %}{% endfor %}', (58, 76)),
-            ('{% load bad_tag %}{% for i in range %}{% badsimpletag %}{% for j in range %}Hello{% endfor %}{% endfor %}', (38, 56)),
-            ('{% load bad_tag %}{% for i in range %}{% for j in five %}{% badsimpletag %}{% endfor %}{% endfor %}', (38, 57)),
-            ('{% load bad_tag %}{% for j in five %}{% badsimpletag %}{% endfor %}', (18, 37)),
+            ('{% load bad_tag %}'
+             '{% for i in range %}'
+             '{% badsimpletag %}'
+             '{% endfor %}',
+             (38, 56)),
+
+            ('{% load bad_tag %}'
+             '{% for i in range %}{% for j in range %}'
+             '{% badsimpletag %}'
+             '{% endfor %}{% endfor %}',
+             (58, 76)),
+
+            ('{% load bad_tag %}'
+             '{% for i in range %}'
+             '{% badsimpletag %}'
+             '{% for j in range %}Hello{% endfor %}'
+             '{% endfor %}',
+             (38, 56)),
+
+            ('{% load bad_tag %}'
+             '{% for i in range %}{% for j in five %}'
+             '{% badsimpletag %}'
+             '{% endfor %}{% endfor %}',
+             (38, 57)),
+
+            ('{% load bad_tag %}'
+             '{% for j in five %}'
+             '{% badsimpletag %}'
+             '{% endfor %}',
+             (18, 37)),
         ]
         context = Context({
             'range': range(5),
             'five': 5,
         })
+        engine = TemplateEngineWithBuiltins()
         for source, expected_error_source_index in tests:
-            template = get_template_from_string(source)
+            template = _Template(engine, source)
             try:
                 template.render(context)
             except (RuntimeError, TypeError) as e:
